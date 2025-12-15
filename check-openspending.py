7745c866e5dc00235c549c0d2037f0e5d594e231
Rob van Dijk <697696+robvandijk@users.noreply.github.com>
2025-12-15 16:34:44 +0100

    Refactors existing code into a class

--- check-openspending.py
+++ check-openspending.py
@@ -6,6 +6,7 @@ import os
 import re
 import requests
 import sys
+import traceback
 
 from datetime import datetime, date, timedelta
 from jsondiff import diff
@@ -21,115 +22,128 @@ from sendgrid import *
 # 'sudo crontab -e' to run it every day:
 #0 0 * * * (cd /home/projects/openspending-check && sudo ./check-openspending.py)
 
+class OpenSpendingChecker:
+    def __init__(self):
+        parser = argparse.ArgumentParser(description=
+            'Checks for new CBS Iv3 updates every day and emails us if new Iv3 data'
+            'is found'
+        )
 
-parser = argparse.ArgumentParser(description=
-    'Checks for new CBS Iv3 updates every day and emails us if new Iv3 data'
-    'is found'
-)
+        parser.add_argument('-t', '--today', action="store", dest="today_date")
+        parser.add_argument('-y', '--yesterday', action="store", dest="yesterday_date")
+        self.args = parser.parse_args()
+        self.log_dir = 'daily-results'
 
-parser.add_argument('-t', '--today', action="store", dest="today_date")
-parser.add_argument('-y', '--yesterday', action="store", dest="yesterday_date")
-args = parser.parse_args()
+        self.today_date = date.today()
+        if self.args.today_date:
+            self.today_date = self.args.today_date
+        self.today_file = f"{self.log_dir}/{self.today_date}.json"
+
+        yesterday_date = date.today() - timedelta(days=1)
+        if self.args.yesterday_date:
+            yesterday_date = self.args.yesterday_date
+        self.yesterday_file = f"{self.log_dir}/{yesterday_date}.json"
+
+    @classmethod
+    def sendmail(cls, subject, content, to=['developers@openstate.eu']):
+        sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_API_KEY)
+        mail = Mail()
+        email = Email()
+        email.email = "developers@openstate.eu"
+        email.name = "Open State Foundation developers"
+        mail.from_email = email
+        mail.subject = subject
+
+        personalization = Personalization()
+        for address in to:
+            personalization.add_to(Email(address))
+        mail.add_personalization(personalization)
+
+        mail.add_content(Content("text/plain", content))
+
+        sg.client.mail.send.post(request_body=mail.get())
+
+    def get_new_data(self):
+        cbs_iv3_catalog = 'https://dataderden.cbs.nl/ODatacatalog/Tables?$format=json'
+
+        s = requests.session()
+
+        result = s.get(cbs_iv3_catalog).json()
+
+        # Save only the items related to Iv3 data
+        all_items = {}
+        for item in result['value']:
+            identifier = item['Identifier']
+            if item['Catalog'] == 'IV3':
+                all_items[identifier] = item
+
+        # Don't write to the today_file if we passed the today_date as an
+        # argument as this means that the file already exists
+        if not self.args.today_date:
+            with open(self.today_file, 'w') as OUT:
+                json.dump(all_items, OUT, indent=4, sort_keys=True)
+
+        try:
+            with open(self.today_file) as IN:
+                return [True, json.load(IN)]
+        except:
+            self.sendmail(
+                '[Open Spending Checker] Could not find today\'s test',
+                'The automated daily check for Open Spending updates could not find today\'s test file \'%s\'' % (self.today_file)
+            )
+            return [False, None]
+
+    def get_old_data(self):
+        try:
+            with open(self.yesterday_file) as IN:
+                return [True, json.load(IN)]
+        except:
+            self.sendmail(
+                '[Open Spending Checker] Could not find yesterday\'s test',
+                'The automated daily check for Open Spending updates could not find yesterday\'s test file \'%s\'' % (self.yesterday_file)
+            )
+            return [False, None]
+
+    def process(self):
+        result, today = self.get_new_data()
+        if not result: return
+        if not today:
+            self.sendmail(
+                '[Open Spending Checker] Today\'s test is empty',
+                'The automated daily check for Open Spending updates could not find results in today\'s test file \'%s\'' % (self.today_file)
+            )
+            return
+
+        result, yesterday = self.get_old_data()
+        if not result: return
+        if not yesterday:
+            self.sendmail( 
+                '[Open Spending Checker] Yesterday\'s test is empty',
+                'The automated daily check for Open Spending updates could not find results in yesterday\'s test file \'%s\'' % (self.yesterday_file)
+            )
+            return
 
 
-def sendmail(subject, content, to=['developers@openstate.eu']):
-    sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_API_KEY)
-    mail = Mail()
-    email = Email()
-    email.email = "developers@openstate.eu"
-    email.name = "Open State Foundation developers"
-    mail.from_email = email
-    mail.subject = subject
-
-    personalization = Personalization()
-    for address in to:
-        personalization.add_to(Email(address))
-    mail.add_personalization(personalization)
-
-    mail.add_content(Content("text/plain", content))
-
-    sg.client.mail.send.post(request_body=mail.get())
-
-log_dir = 'daily-results'
-cbs_iv3_catalog = 'https://dataderden.cbs.nl/ODatacatalog/Tables?$format=json'
-
-s = requests.session()
-
-result = s.get(cbs_iv3_catalog).json()
-
-# Save only the items related to Iv3 data
-all_items = {}
-for item in result['value']:
-    identifier = item['Identifier']
-    if item['Catalog'] == 'IV3':
-        all_items[identifier] = item
-
-today_date = date.today()
-if args.today_date:
-    today_date = args.today_date
-yesterday_date = date.today() - timedelta(days=1)
-if args.yesterday_date:
-    yesterday_date = args.yesterday_date
-
-today_file = f"{log_dir}/{today_date}.json"
-yesterday_file = f"{log_dir}/{yesterday_date}.json"
-
-# Don't write to the today_file if we passed the today_date as an
-# argument as this means that the file already exists
-if not args.today_date:
-    with open(today_file, 'w') as OUT:
-        json.dump(all_items, OUT, indent=4, sort_keys=True)
+        # Format of changes:
+        #   - keys are the dataset names, e.g. '45005NED', '45006NED', '45007NED'
+        #   - if an insert or delete has taken place, the key is an object so e.g. {insert: ['a']}
+        #   - pass 'marshal="True"' to diff to turn those keys into strings prepended by a '$', so the above would become {'$insert': ['a']}
+        changes = diff(yesterday, today, syntax='symmetric')
+        if changes:
+            self.sendmail(
+                '[Open Spending Checker] Found changes in CBS data, time to update!',
+                'The automated daily check for Open Spending updates detected the following changes in the test results compared to yesterday:\n\n%s' % (pformat(changes, indent=2))
+            )
 
 try:
-    with open(today_file) as IN:
-        today = json.load(IN)
-except:
-    sendmail(
-        '[Open Spending Checker] Could not find today\'s test',
-        'The automated daily check for Open Spending updates could not find today\'s test file \'%s\'' % (today_file)
-    )
-    exit()
-
-try:
-    with open(yesterday_file) as IN:
-        yesterday = json.load(IN)
-except:
-    sendmail(
-        '[Open Spending Checker] Could not find yesterday\'s test',
-        'The automated daily check for Open Spending updates could not find yesterday\'s test file \'%s\'' % (yesterday_file)
-    )
-    exit()
-
-if not today:
-    sendmail(
-        '[Open Spending Checker] Today\'s test is empty',
-        'The automated daily check for Open Spending updates could not find results in today\'s test file \'%s\'' % (today_file)
-    )
-    exit()
-
-if not yesterday:
-    sendmail(
-        '[Open Spending Checker] Yesterday\'s test is empty',
-        'The automated daily check for Open Spending updates could not find results in yesterday\'s test file \'%s\'' % (yesterday_file)
-    )
-    exit()
-
-def mail_error(cmd):
-    sendmail(
-        '[Open Spending Checker] Error when updating',
-        'The automated daily check for Open Spending updates found changes but ran into an error when it tried to automatically update using the following command:\n\n%s\n\nCheck the log file for the actual error message.' % (cmd)
+    checker = OpenSpendingChecker()
+    checker.process()
+except Exception as e:
+    OpenSpendingChecker.sendmail(
+        '[Open Spending Checker] Exception occurred',
+        f'{str(e)} {traceback.print_tb(e.__traceback__)}'
     )
 
-# Format of changes:
-#   - keys are the dataset names, e.g. '45005NED', '45006NED', '45007NED'
-#   - if an insert or delete has taken place, the key is an object so e.g. {insert: ['a']}
-#   - pass 'marshal="True"' to diff to turn those keys into strings prepended by a '$', so the above would become {'$insert': ['a']}
-changes = diff(yesterday, today, syntax='symmetric')
-if changes:
-    sendmail(
-        '[Open Spending Checker] Found changes in CBS data, time to update!',
-        'The automated daily check for Open Spending updates detected the following changes in the test results compared to yesterday:\n\n%s' % (pformat(changes, indent=2))
-    )
 
 # Code for old Open Spending
 ## New datasets (i.e., year) are added using '$insert' in changes so
